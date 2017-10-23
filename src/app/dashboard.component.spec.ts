e39346e138b51973aae981747c50a5e26e630609
egusahiroaki <egusa.hiroaki@gmail.com>
2017-10-23 23:26:21 +0900

    add test case 2

--- src/app/dashboard.component.spec.ts
+++ src/app/dashboard.component.spec.ts
@@ -1,41 +1,67 @@
 import { TestBed, async } from '@angular/core/testing';
-
 import { DashboardComponent } from './dashboard.component';
+import { By } from '@angular/platform-browser';
+
 import { HeroService } from './hero.service';
 import { Hero } from './hero';
+import { ComponentFixture } from '@angular/core/testing';
+import { DebugElement } from '@angular/core';
+
+import { RouterTestingModule } from '@angular/router/testing';
+import { HEROES } from './mock-heroes';
+
+describe('Dashboard Component', () => {
+  let service: HeroService;
+  let comp: DashboardComponent;
+  let fixture: ComponentFixture<DashboardComponent>;
+  let de: DebugElement;
 
 
-describe('Dashboard Test', () => {
-
-  let HeroServiceStub: {
-    getHeroes(): Promise<Hero[]>;
-  };
-
+  // load & compile template 
   beforeEach(async(() => {
 
-    HeroServiceStub.getHeroes
+    // HeroServiceStub
+    let serviceStub = {
+      getHeroes() {
+        return Promise.resolve(HEROES);
+      }
+    };
 
     TestBed.configureTestingModule({
-      declarations: [
-        DashboardComponent
-      ],
-      providers: [ {provide: HeroService, useValue: HeroServiceStub } ]
-    }).compileComponents();
+      imports: [ RouterTestingModule],
+      declarations: [ DashboardComponent ]
+    }).
+    // override component configuration
+    overrideComponent(DashboardComponent, {
+      set: {
+        providers: [
+          { provide: HeroService, useValue: serviceStub}
+        ]
+      }
+    });
+
+    // create component instance
+    fixture = TestBed.createComponent(DashboardComponent);
+    comp = fixture.componentInstance;
+
+    // get Dependency Service
+    service = fixture.debugElement.injector.get(HeroService);
   }));
-  it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(DashboardComponent);
+
+  it('should create the dashboard component', async(() => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  // it(`should have as title 'app'`, async(() => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   const app = fixture.debugElement.componentInstance;
-  //   expect(app.title).toEqual('app');
-  // }));
-  // it('should render title in a h1 tag', async(() => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   fixture.detectChanges();
-  //   const compiled = fixture.debugElement.nativeElement;
-  //   expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
-  // }));
+
+  it('should have hero names', async(() => {
+    fixture.detectChanges(); // THIS IS IMPORTANT!!
+    let des = fixture.debugElement.queryAll(By.css('h4'));
+    expect(des.length).toEqual(0);
+
+    fixture.whenStable().then(() => {
+      fixture.detectChanges();
+      des = fixture.debugElement.queryAll(By.css('h4'));
+      expect(des.length).toEqual(10);
+    });
+  }));
 });
