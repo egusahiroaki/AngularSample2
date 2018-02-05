40cd61a6626cd8e001e3125f615684f274439cc1
egusahiroaki <egusa.hiroaki@gmail.com>
2018-02-05 23:37:59 +0900

    追加

---
+++ src/app/hero-detail.component.spec.ts
@@ -0,0 +1,19 @@
+import { TestBed, async } from '@angular/core/testing';
+import { HeroDetailComponent } from './hero-detail.component';
+
+import { HeroService } from './hero.service';
+import { ComponentFixture } from '@angular/core/testing';
+import { DebugElement } from '@angular/core';
+
+
+describe('HeroDetail Component', () => {
+  let service: HeroService;
+  let comp: HeroDetailComponent;
+  let fixture: ComponentFixture<HeroDetailComponent>;
+  let de: DebugElement;
+
+  beforeEach(() => {
+    
+  });
+
+});
